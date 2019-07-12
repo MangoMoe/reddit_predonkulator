f90f8343f5e084d9b628bc3a4924d1371335d892
Dallin Clayton <dallin.clayton@gmail.com>
2019-07-12 10:39:38 -0600

    Organization and get rid of scrapy

--- reddit_test/reddit_test/misc/reddit_post.py
+++
@@ -1,9 +0,0 @@
-
-class reddit_post:
-    # Get title, body text, image, links, num upvotes
-    def __init__(self, title, body, img, links, upvotes):
-        self.title = title
-        self.body = body
-        self.img = img
-        self.links = links
-        self.upvotes = upvotes
