4bc34382576dc29f2e1cd3685df9cb8b9e54733d
Dallin Clayton <dallin.clayton@gmail.com>
2019-07-15 12:27:38 -0600

    I totally spaced this stuff

--- oauth_method/api_flask_url.py
+++ oauth_method/api_flask_url.py
@@ -35,32 +35,8 @@ def reddit_callback():
     code = request.args.get('code')
     accessor.apply_authorization_code(code)
     return "got a code! %s" % code
-    # return
 
 if __name__ == '__main__':
     manager.run()
     # app.run(debug=True, port=9001)
     print("test?")
-
-
-# from flask import Flask
-# from flask_script import Manager, Server
-
-# def custom_call():
-# #Your code
-# pass
-
-# class CustomServer(Server):
-# def __call__(self, app, *args, **kwargs):
-#     custom_call()
-#     #Hint: Here you could manipulate app
-#     return Server.__call__(self, app, *args, **kwargs)
-
-# app = Flask(__name__)
-# manager = Manager(app)
-
-# # Remeber to add the command to your Manager instance
-# manager.add_command('runserver', CustomServer())
-
-# if __name__ == "__main__":
-# manager.run()
