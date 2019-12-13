1b4c863d8e55439be77d26754c85ff2d31b5da45
Dallin Clayton <dallin.clayton@gmail.com>
2019-12-13 14:56:57 -0700

    Got regex with context working (words and lines)

--- app/corpus/searcher.py
+++ app/corpus/searcher.py
@@ -4,16 +4,28 @@ import re
 with open("halo_corpus_file.corp", encoding="utf-8") as corpus_file:
     # expression = re.compile(".*halo.*", re.IGNORECASE)
     num_surrounding_words = 3
-    search_term = "halo"
+    search_term = "reach"
     word_regex_before = r'\w+\s' * num_surrounding_words
     word_regex_after = r'\s\w+' * num_surrounding_words
-    pat = re.compile(r'(' + word_regex_before + search_term + word_regex_after + r')')
+    pat_context = re.compile(r'(' + word_regex_before + search_term + word_regex_after + r')', re.IGNORECASE)
     # pat = re.compile(r'(\w+\s\w+\shalo\s\w+\s\w+)')
     corpus = corpus_file.read()
-    hits = pat.findall(corpus)
+    hits = pat_context.findall(corpus)
     for hit in hits:
         print(hit)
     print(len(hits))
+    pat_word = re.compile(search_term, re.IGNORECASE)
+    # pat_word_newline = re.compile(r'\r\n.*\r\n.*search_term.*\r\n.*\r\n', re.IGNORECASE)
+    pat_word_newline = re.compile(r"^(.*\n.*" + search_term + r".*\n.*)$", re.MULTILINE)
+    iterator = re.finditer(pat_word_newline, corpus)
+    count = 0
+    for match in iterator:
+        count +=1
+        # print(match.span())
+        # print("-------")
+        # print(corpus[match.span()[0]: match.span()[1]])
+        # print("-------")
+    print(count)
     # query = re.search(".*Halo.*", corpus)
     # print(query.group())
 
